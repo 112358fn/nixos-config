edefdff538a3817be9592222d6d7b79c957ffbe5
Alvaro Alonso <112358.fn@gmail.com>
2023-08-04 16:15:43 +0200

    Add alacritty config

--- user/alvaro/home-manager.nix
+++ user/alvaro/home-manager.nix
@@ -5,7 +5,7 @@
     enable = true;
     configFile."i3/config".text = builtins.readFile ./i3;
     configFile."nvim".source = ./nvim;
-    #configFile."rofi/config.rasi".text = builtins.readFile ./rofi;
+    configFile."alacritty".source = ./alacritty;
   };
 
   home = {
@@ -40,29 +40,30 @@
   programs = {
     home-manager.enable = true;
     password-store.enable = true;
+    alacritty.enable = true;
     gpg = {
       enable = true;
       settings = {
         personal-cipher-preferences = "AES256 AES192 AES";
-	personal-digest-preferences = "SHA512 SHA384 SHA256";
-	personal-compress-preferences = "ZLIB BZIP2 ZIP Uncompressed";
-	default-preference-list = "SHA512 SHA384 SHA256 AES256 AES192 AES ZLIB BZIP2 ZIP Uncompressed";
-	cert-digest-algo = "SHA512";
-	s2k-digest-algo = "SHA512";
-	s2k-cipher-algo = "AES256";
-	charset = "utf-8";
-	fixed-list-mode = true;
-	no-comments = true;
-	no-emit-version = true;
-	no-greeting = true;
-	keyid-format = "0xlong";
-	list-options = "show-uid-validity";
-	verify-options = "show-uid-validity";
-	with-fingerprint = true;
-	require-cross-certification = true;
-	no-symkey-cache = true;
-	use-agent = true;
-	throw-keyids = true;
+        personal-digest-preferences = "SHA512 SHA384 SHA256";
+        personal-compress-preferences = "ZLIB BZIP2 ZIP Uncompressed";
+        default-preference-list = "SHA512 SHA384 SHA256 AES256 AES192 AES ZLIB BZIP2 ZIP Uncompressed";
+        cert-digest-algo = "SHA512";
+        s2k-digest-algo = "SHA512";
+        s2k-cipher-algo = "AES256";
+        charset = "utf-8";
+        fixed-list-mode = true;
+        no-comments = true;
+        no-emit-version = true;
+        no-greeting = true;
+        keyid-format = "0xlong";
+        list-options = "show-uid-validity";
+        verify-options = "show-uid-validity";
+        with-fingerprint = true;
+        require-cross-certification = true;
+        no-symkey-cache = true;
+        use-agent = true;
+        throw-keyids = true;
       };
     };
     git = {
@@ -70,28 +71,17 @@
       userName = "Alvaro Alonso";
       userEmail = "112358.fn@gmail.com";
       extraConfig = {
-	init.defaultBranch = "main";
-	push.default = "current";
+        init.defaultBranch = "main";
+        push.default = "current";
       };
       #signing = true;
     };  
-    alacritty = {
-      enable = true;
-      settings = {
-	font = {
-	  size = 12;
-	  draw_bold_text_with_bright_colors = true;
-	};
-	scrolling.multiplier = 5;
-	selection.save_to_clipboard = true;
-      };
-    };
     zsh = {
       enable = true;
       oh-my-zsh = {
-	enable = true;
-	theme = "robbyrussell";
-	plugins = ["git"];
+        enable = true;
+        theme = "robbyrussell";
+        plugins = ["git"];
       };
     };
     direnv = {
