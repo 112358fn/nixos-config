66396b9e190190a36d743cac78bc135ff4e56f95
Alvaro Alonso <112358.fn@gmail.com>
2023-08-07 23:43:27 +0200

    Add userChrome to firefox

--- user/alvaro/home-manager/firefox/default.nix
+++ user/alvaro/home-manager/firefox/default.nix
@@ -1,5 +1,12 @@
 { ... }: {
   programs.firefox = {
     enable = true;
+    profiles.personal = {
+      isDefault = true;
+      userChrome = with builtins;concatStringsSep "\n" (map (f: readFile ./chrome/includes/${f}) (attrNames (readDir ./chrome/includes)));
+      settings = {
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+      };
+    };
   };
 }
