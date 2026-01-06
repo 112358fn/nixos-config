d5dfd6a339090580fa444aaaa0354fe69a8b7ba0
Alvaro Alonso <112358.fn@gmail.com>
2026-01-06 12:21:03 +0100

    machine/macbookair: wayland wm

--- machine/macbookair/default.nix
+++ machine/macbookair/default.nix
@@ -1,10 +1,19 @@
 { config, pkgs, ... }:
 
 {
+  imports = [
+    ./wm.nix
+  ];
   system.stateVersion = "23.05";
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  nixpkgs.config.allowUnfree = true;
+
+  nixpkgs.config = {
+    allowUnfree = true;
+    permittedInsecurePackages = [
+        "broadcom-sta-6.30.223.271-57-6.12.41"
+      ];
+  };
 
   boot.loader = {
     systemd-boot.enable = true;
@@ -16,19 +25,6 @@
     networkmanager.enable = true;
   };
 
-  environment.systemPackages = with pkgs; [
-    xss-lock
-    i3lock-color
-    i3status-rust
-  ];
-  # XDG Portal is needed for flatpak
-  xdg.portal = {
-    enable = true;
-    config.common.default = "gtk";
-    extraPortals = [
-     pkgs.xdg-desktop-portal-gtk 
-    ];
-  };
   services = {
     # Map CapsLock to Esc on single press and Ctrl on when used with multiple keys.
     interception-tools = {
@@ -42,23 +38,12 @@
               EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
       '';
     };
-    # Flatpak is used to install zen browser
-    # until it is included in nixpkgs
-    flatpak.enable = true;
-    xserver = {
-      enable = true;
-      windowManager.i3.enable = true;
-      displayManager.gdm.enable = true;
-    };
     pcscd.enable = true;
     dbus.packages = [ pkgs.gcr ];
     logind.extraConfig = ''
       HandlePowerKey=ignore
       HoldoffTimeoutSec=0s
     '';
-    # udev.extraRules = ''
-    #   ACTION=="change", SUBSYSTEM=="drm", ENV{DISPLAY}=":0", ENV{XAUTHORITY}="/run/user/1000/gdm/Xauthority", RUN+="${pkgs.xorg.xrandr}/bin/xrandr --auto"
-    # '';
   };
 
   time.timeZone = "Europe/Stockholm";
---
+++ machine/macbookair/wm.nix
@@ -0,0 +1,46 @@
+{pkgs, ... }: {
+  
+  environment.systemPackages = with pkgs; [
+    networkmanagerapplet
+    blueberry
+    whitesur-gtk-theme
+    whitesur-icon-theme
+    whitesur-cursors
+    nwg-look
+    nautilus
+    gnome-calendar
+    gnome-contacts
+    gnome-online-accounts-gtk
+    slack
+    ungoogled-chromium
+  ];
+  environment.sessionVariables = {
+    GSK_RENDERER = "ngl";
+    NIXOS_OZONE_WL = "1";
+  };
+  programs = {
+    sway = {
+      enable = true;
+      wrapperFeatures.gtk = true;
+      extraPackages = with pkgs;[
+        waybar
+        tofi
+        swayidle
+        swaylock
+        swaynotificationcenter
+        brightnessctl
+        pavucontrol
+        killall
+        wl-clipboard
+      ];
+    };
+    geary.enable = true;
+  };
+  # Flatpak is used to install zen browser
+  # until it is included in nixpkgs
+  services.flatpak.enable = true;
+  # Evolution data server is used
+  # by calendar and notes
+  services.gnome.evolution-data-server.enable = true;
+  services.gnome.gnome-keyring.enable = true;
+}
