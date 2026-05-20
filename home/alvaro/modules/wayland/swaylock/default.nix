6bf05a616f6c1ca13598482bdcd0c4e9a8a95205
Alvaro Alonso Bivou <112358.fn@gmail.com>
2026-05-20 13:22:24 +0200

    home/swaylock: fixing lid lock
    Signed-off-by: Alvaro Alonso Bivou <112358.fn@gmail.com>

--- home/alvaro/modules/wayland/swaylock/default.nix
+++ home/alvaro/modules/wayland/swaylock/default.nix
@@ -5,9 +5,11 @@
     settings = { };
   };
   xdg.configFile."swaylock/config".source = ./config;
+  # We are not using the logind because of
+  # https://whynothugo.nl/journal/2022/10/26/systemd-locking-and-sleeping/
   xdg.configFile."sway/config.d/3_lock".text = ''
-    set $lock swaylock -f && systemctl suspend
-    bindswitch --locked lid:toggle exec $lock
-    bindsym Mod1+Ctrl+q exec $lock
+    set $lock pgrep swaylock || swaylock -f && systemctl suspend
+    bindswitch --locked lid:on exec $lock
+    bindsym --locked Mod1+Ctrl+q exec $lock
   '';
 }
