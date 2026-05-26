0226f39fdcc1e04f4b52ad6f3da697130d416090
Alvaro Alonso Bivou <112358.fn@gmail.com>
2026-05-26 17:26:48 +0200

    home/alvaro: firefox inhibit idle
    Signed-off-by: Alvaro Alonso Bivou <112358.fn@gmail.com>

--- home/alvaro/modules/wayland/xdg.nix
+++ home/alvaro/modules/wayland/xdg.nix
@@ -6,7 +6,19 @@
       pkgs.xdg-desktop-portal-gtk
       pkgs.xdg-desktop-portal-wlr
     ];
-    config.common.default = "wlr";
+    # https://github.com/emersion/xdg-desktop-portal-wlr/blob/master/contrib/wlroots-portals.conf
+    # https://github.com/emersion/xdg-desktop-portal-wlr/pull/315
+    config.sway = {
+      # Use xdg-desktop-portal-gtk for every portal interface...
+      default = "gtk";
+      # ... except for the ScreenCast, Screenshot and Secret
+      "org.freedesktop.impl.portal.ScreenCast" = "wlr";
+      "org.freedesktop.impl.portal.Screenshot" = "wlr";
+      # ignore inhibit bc gtk portal always returns as success,
+      # despite sway/the wlr portal not having an implementation,
+      # stopping firefox from using wayland idle-inhibit
+      "org.freedesktop.impl.portal.Inhibit" = "none";
+    };
     xdgOpenUsePortal = true;
   };
 
