0193c3a7873836090fc54f43516a3e5ee0b796b1
Alvaro Alonso Bivou <112358.fn@gmail.com>
2026-06-09 09:35:40 +0200

    home/wayland: dropdown terminal
    Signed-off-by: Alvaro Alonso Bivou <112358.fn@gmail.com>

--- home/alvaro/modules/wayland/terminal.nix
+++ home/alvaro/modules/wayland/terminal.nix
@@ -10,6 +10,15 @@
     enable = true;
     server.enable = true;
   };
-  xdg.configFile."sway/config.d/4_terminal".text = "bindsym $mod+Return exec footclient";
+  programs.zellij = {
+    enable = true;
+    enableFishIntegration = true;
+    attachExistingSession = true;
+    exitShellOnExit = true;
+  };
+  xdg.configFile."sway/config.d/4_terminal".text = ''
+    for_window [app_id="footclient"] move scratchpad
+    bindsym $mod+grave exec 'swaymsg "[app_id=footclient] nop" || footclient -N && swaymsg "[app_id=footclient] scratchpad show, resize set 100ppt 100ppt, move position 0 0"'
+  '';
   home.sessionVariables.TERMINAL = "footclient";
 }
