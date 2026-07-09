c04c3bea3b9c583dd4c961ccac1275d1b977a400
Alvaro Alonso Bivou <112358.fn@gmail.com>
2026-07-09 15:06:58 +0200

    nixos/alvaro: add udev rule for nuphy kb

--- nixos/users/alvaro/nixos.nix
+++ nixos/users/alvaro/nixos.nix
@@ -9,11 +9,13 @@
       "networkmanager"
       "dialout"
       "docker"
+      "hidraw"
     ];
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDfDofWEXwuMRtaXI4Vk6/hla4w53/gMbvknPc7CSaFC3sv4gqBogreWJyJ62ZpwnClxhmM9k4J2gdkZlV56m3gbbKaLaLCSOjPoLYMy+2ruiyRrfhSHFJFFJJCBKgPN9GSE3xi53j4WFPCOZ6prA6EzRzYAWV0sZsoIHo9NEI6kdkWa8d7lH43zPcL9+qIA9hwPs2cZ/1YMXFSGeLpJPpNv0F7JGgqLtWY1kN7dd1jMPAwM1hFCi9SQj0SNurntT8WNlsZoJks+c3OvFzvgfFgHSOOjSacjFNRJE1qi8BWjK++cA2940hj1the3CKFA8c8PVMM6zWOHIR/Y6W3V0YFQj3/SIldFFLrr4OffOHTNMZsrRaVK1zWpUPRGYNNW4AUBh37eUvUkkSv8BRwU4snZzBT3FVLJZMpVkWUOjPHmFeoS9DQ9CrI1uaArYMrj/i+O2c9ntZWyZ97hVHWx8n3OkwkOfC2UeSNOuW2RIbODGH7vPZnHJhrYxMDynwh3dIVkdwdDbxIbUCl+s9Fu9fofKf5FeAYkWpY1cBQpKOh2U5hR/42O0i76RlVAsom361NfZ+Sx25RkHQ5p6yKJ4m8zYypmwf8WdsnIXd8xJxxd8j4we4YsCR2AbCY/UVx+S2/L6BmidqqbHF11UZU2azD5VOOiazLNhyCrUsRk9LYkw== cardno:23_869_830"
     ];
   };
+  users.groups.hidraw = {};
 
   programs.dconf.enable = true;
   # Needed to use uv
@@ -78,7 +80,10 @@
     };
     # Upower
     upower.enable = true;
+    udev.extraRules = ''
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", GROUP="hidraw", MODE="0660"
+    '';
   };
   powerManagement.powertop.enable = true;
-  systemd.services.tailscaled.wantedBy = lib.mkForce [];
+  systemd.services.tailscaled.wantedBy = lib.mkForce [ ];
 }
