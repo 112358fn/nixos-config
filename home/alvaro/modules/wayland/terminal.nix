67ff17172a40ba54249ce8d2cefe571b97f0d1ef
Alvaro Alonso Bivou <112358.fn@gmail.com>
2026-07-11 11:15:44 +0200

    home/alvaro: rm zellij
    Signed-off-by: Alvaro Alonso Bivou <112358.fn@gmail.com>

--- home/alvaro/modules/wayland/terminal.nix
+++ home/alvaro/modules/wayland/terminal.nix
@@ -2,7 +2,7 @@
 {
   programs.ghostty = {
     enable = true;
-    systemd.enable = true;
+    systemd.enable = false;
     enableBashIntegration = true;
   };
 
@@ -10,15 +10,16 @@
     enable = true;
     server.enable = true;
   };
-  programs.zellij = {
-    enable = true;
-    enableFishIntegration = true;
-    attachExistingSession = true;
-    exitShellOnExit = true;
-  };
+  #programs.zellij = {
+  #  enable = true;
+  #  enableFishIntegration = true;
+  #  attachExistingSession = true;
+  #  exitShellOnExit = true;
+  #};
   xdg.configFile."sway/config.d/4_terminal".text = ''
-    for_window [app_id="footclient"] move scratchpad
-    bindsym $mod+grave exec 'swaymsg "[app_id=footclient] nop" || footclient -N && swaymsg "[app_id=footclient] scratchpad show, resize set 100ppt 100ppt, move position 0 0"'
+    assign [app_id="footclient"] workspace $
+    bindsym $mod+grave exec 'swaymsg "[app_id=footclient] nop" || footclient -N && swaymsg "workspace \$"'
+    bindsym $mod+return exec 'footclient -N'
   '';
   home.sessionVariables.TERMINAL = "footclient";
 }
