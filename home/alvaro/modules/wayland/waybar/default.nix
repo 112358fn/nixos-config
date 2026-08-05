3c596674d6310549c38d8250f3f69153b5f193af
Alvaro Alonso Bivou <112358.fn@gmail.com>
2026-08-05 17:13:29 +0200

    home/waybar: jsonc to nix config

--- home/alvaro/modules/wayland/waybar/default.nix
+++ home/alvaro/modules/wayland/waybar/default.nix
@@ -2,10 +2,25 @@
     programs.waybar = {
       enable = true;
       systemd.enable = true;
+      settings.mainBar = {
+        layer = "bottom";
+        modules-left = [ "sway/workspaces" "sway/mode" ];
+        modules-center = [ ];
+        modules-right = [ "tray" "network" "battery" "clock" ];
+        network = {
+          format-wifi = "{signalStrength}% ";
+          format-ethernet = "{ifname}";
+          tooltip-format = "{ipaddr}";
+          format-linked = "{ifname} (No IP)";
+          format-disconnected = "Disconnected ⚠";
+          format-alt = "{ifname} {essid}";
+        };
+        battery = {
+          format = "{capacity}% {icon}  {power:.2}W";
+          format-icons = [ "" "" "" "" "" ];
+        };
+        clock.format-alt = "{:%a, %d. %b  %H:%M}";
+      };
+      style = ./style.css;
     };
-    xdg.configFile = {
-    "waybar/config.jsonc".source = ./config.jsonc;
-    "waybar/style.css".source = ./style.css;
-    };
-
 }
