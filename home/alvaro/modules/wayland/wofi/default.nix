3db60e6485bada4ad99199be8931cd9d093e0e08
Alvaro Alonso Bivou <112358.fn@gmail.com>
2026-08-23 19:32:15 +0200

    home/alvaro: toggle wofi drun

--- home/alvaro/modules/wayland/wofi/default.nix
+++ home/alvaro/modules/wayland/wofi/default.nix
@@ -2,7 +2,7 @@
 {
   programs.wofi.enable = true;
   xdg.configFile."sway/config.d/3_menu".text = ''
+    set $menu pkill wofi || wofi --show=drun
     bindsym $mod+d exec $menu
-    set $menu pkill --exact wofi || wofi --show=drun
   '';
 }
