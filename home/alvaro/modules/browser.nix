0ee4d24eb6b678968b8cfb2342989164c3e99249
Alvaro Alonso Bivou <112358.fn@gmail.com>
2026-09-02 08:53:06 +0200

    home/alvaro: google meet URL open PWA

--- home/alvaro/modules/browser.nix
+++ home/alvaro/modules/browser.nix
@@ -10,4 +10,38 @@
     nativeMessagingHosts = [ pkgs.unstable.passff-host ];
     configPath = "${config.xdg.configHome}/mozilla/firefox";
   };
+  home.packages = [
+    (pkgs.writeShellScriptBin "browser-dispatch" ''
+      case "$1" in
+        *meet.google.com*)
+          exec ${pkgs.unstable.ungoogled-chromium}/bin/chromium --app="$1"
+          ;;
+        *)
+          exec ${pkgs.firefox}/bin/firefox "$1"
+          ;;
+      esac
+    '')
+  ];
+  xdg.desktopEntries.browser-dispatch = {
+    name = "Browser Dispatcher";
+    exec = "browser-dispatch %u";
+    categories = [
+      "Network"
+      "WebBrowser"
+    ];
+    mimeType = [
+      "x-scheme-handler/http"
+      "x-scheme-handler/https"
+      "text/html"
+    ];
+  };
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "x-scheme-handler/http" = "browser-dispatch.desktop";
+      "x-scheme-handler/https" = "browser-dispatch.desktop";
+      "text/html" = "browser-dispatch.desktop";
+    };
+  };
 }
