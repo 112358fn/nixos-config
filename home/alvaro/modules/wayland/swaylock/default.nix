7f27e521c2dfcba93f9db217fec54b0b84381578
Alvaro Alonso Bivou <112358.fn@gmail.com>
2026-09-02 08:45:46 +0200

    home/alvaro: lock with swayidle dissabled

--- home/alvaro/modules/wayland/swaylock/default.nix
+++ home/alvaro/modules/wayland/swaylock/default.nix
@@ -5,10 +5,7 @@
     settings = { };
   };
   xdg.configFile."swaylock/config".source = ./config;
-  # Lid-close suspend is handled by logind (nixos/hosts/framework/hw.nix)
-  # so the machine also sleeps outside a sway session and re-suspends after
-  # spurious wakes; swayidle's before-sleep locks the screen first.
   xdg.configFile."sway/config.d/3_lock".text = ''
-    bindsym --locked Mod1+Ctrl+q exec systemctl suspend
+    bindsym --locked Mod1+Ctrl+q exec swaylock -f
   '';
 }
