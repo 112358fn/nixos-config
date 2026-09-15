7663e1e44bc29ac45fb1c1fdd289578d47388be2
Alvaro Alonso Bivou <112358.fn@gmail.com>
2026-09-15 08:38:50 +0200

    home/swayidle: remove timeouts

--- home/alvaro/modules/wayland/swayidle/default.nix
+++ home/alvaro/modules/wayland/swayidle/default.nix
@@ -2,12 +2,6 @@
 {
   services.swayidle = {
     enable = true;
-    timeouts = [
-      {
-        timeout = 330;
-        command = "${pkgs.systemd}/bin/systemctl suspend";
-      }
-    ];
     events.before-sleep = "${pkgs.swaylock}/bin/swaylock -f";
   };
 }
