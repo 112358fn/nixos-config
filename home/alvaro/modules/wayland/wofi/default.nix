334e2b1e2aa910135d8fab39e96e1d988d325e13
Alvaro Alonso Bivou <112358.fn@gmail.com>
2026-09-19 13:03:28 +0200

    home/wayland: wofi as overlay layer

--- home/alvaro/modules/wayland/wofi/default.nix
+++ home/alvaro/modules/wayland/wofi/default.nix
@@ -1,6 +1,9 @@
 { ... }:
 {
-  programs.wofi.enable = true;
+  programs.wofi = {
+    enable = true;
+    settings.layer = "overlay";
+  };
   xdg.configFile."sway/config.d/3_menu".text = ''
     set $menu pkill wofi || wofi --show=drun
     bindsym $mod+d exec $menu
